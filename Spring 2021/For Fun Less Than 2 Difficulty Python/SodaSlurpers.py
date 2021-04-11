15006254d665a5978f3e71d154eae6f612a0007a
Carter Schmidt <72340761+cschmi14@users.noreply.github.com>
2021-04-11 10:55:34 -0400

    Update SodaSlurpers.py

--- Spring 2021/For Fun Less Than 2 Difficulty Python/SodaSlurpers.py
+++ Spring 2021/For Fun Less Than 2 Difficulty Python/SodaSlurpers.py
@@ -1,1 +1,17 @@
+def findSodas(b, c):
+    total = b / c
+    return total
 
+str = input().split()
+e = int(str[0])
+f = int(str[1])
+c = int(str[2])
+b = f + e
+count = 0
+
+while findSodas(b, c) >= 1:
+    curr = int(findSodas(b,c))
+    count += curr
+    b = curr + (b % c)
+
+print(count)
